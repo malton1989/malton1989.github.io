2ba13e7d3037af26aabaffc5ff08edd0121a13a2
Anton <tosha.malyamov@yandex.ru>
2017-11-25 15:17:22 +0300

    changes

---
+++ js/index.js
@@ -0,0 +1,126 @@
+jQuery(document).ready(function($){
+	var overlayNav = $('.cd-overlay-nav'),
+		overlayContent = $('.cd-overlay-content'),
+		navigation = $('.cd-primary-nav'),
+		toggleNav = $('.cd-nav-trigger');
+  
+  
+  
+
+	//inizialize navigation and content layers
+	layerInit();
+	$(window).on('resize', function(){
+		window.requestAnimationFrame(layerInit);
+	});
+
+	//open/close the menu and cover layers
+	toggleNav.on('click', function(){
+		if(!toggleNav.hasClass('close-nav')) {
+			//it means navigation is not visible yet - open it and animate navigation layer
+			toggleNav.addClass('close-nav');
+			
+			overlayNav.children('span').velocity({
+				translateZ: 0,
+				scaleX: 1,
+				scaleY: 1,
+			}, 500, 'easeInCubic', function(){
+				navigation.addClass('fade-in');
+			});
+		} else {
+			//navigation is open - close it and remove navigation layer
+			toggleNav.removeClass('close-nav');
+			
+			overlayContent.children('span').velocity({
+				translateZ: 0,
+				scaleX: 1,
+				scaleY: 1,
+			}, 500, 'easeInCubic', function(){
+				navigation.removeClass('fade-in');
+				
+				overlayNav.children('span').velocity({
+					translateZ: 0,
+					scaleX: 0,
+					scaleY: 0,
+				}, 0);
+				
+				overlayContent.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					overlayContent.children('span').velocity({
+						translateZ: 0,
+						scaleX: 0,
+						scaleY: 0,
+					}, 0, function(){overlayContent.removeClass('is-hidden')});
+				});
+				if($('html').hasClass('no-csstransitions')) {
+					overlayContent.children('span').velocity({
+						translateZ: 0,
+						scaleX: 0,
+						scaleY: 0,
+					}, 0, function(){overlayContent.removeClass('is-hidden')});
+				}
+			});
+		}
+	});
+
+	function layerInit(){
+		var diameterValue = (Math.sqrt( Math.pow($(window).height(), 2) + Math.pow($(window).width(), 2))*2);
+		overlayNav.children('span').velocity({
+			scaleX: 0,
+			scaleY: 0,
+			translateZ: 0,
+		}, 50).velocity({
+			height : diameterValue+'px',
+			width : diameterValue+'px',
+			top : -(diameterValue/2)+'px',
+			left : -(diameterValue/2)+'px',
+		}, 0);
+
+		overlayContent.children('span').velocity({
+			scaleX: 0,
+			scaleY: 0,
+			translateZ: 0,
+		}, 50).velocity({
+			height : diameterValue+'px',
+			width : diameterValue+'px',
+			top : -(diameterValue/2)+'px',
+			left : -(diameterValue/2)+'px',
+		}, 0);
+	}
+});
+
+
+
+
+
+
+
+/* Twitter feed code
+ *
+ */
+!(function (d, s, id) {
+  var js = d.getElementsByTagName(s)[0]
+  var fjs = d.getElementsByTagName(s)[0]
+  var p = /^http:/.test(d.location) ? 'http' : 'https'
+  if (!d.getElementById(id)) {
+    js = d.createElement(s)
+    js.id = id
+    js.src = p + '://platform.twitter.com/widgets.js'
+    fjs.parentNode.insertBefore(js, fjs)
+  }
+})(document, 'script', 'twitter-wjs');
+//Smooth Anthor
+function scrollNav() {
+  $('.nav a').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 40
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
--- malton-page.markdown
+++
@@ -1,7 +0,0 @@
-Malton page
------------
-
-
-A [Pen](https://codepen.io/webspinner1989/pen/rjEJpY) by [Антон](https://codepen.io/webspinner1989) on [CodePen](https://codepen.io).
-
-[License](https://codepen.io/webspinner1989/pen/rjEJpY/license).
